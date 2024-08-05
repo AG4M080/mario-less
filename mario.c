05f9f6f7320008a1b4006d15ebd8521845809d45
AG4M080 <afgansyakuro080@gmail.com>
2024-08-05 09:35:17 +0700

    Update mario.c

--- mario.c
+++ mario.c
@@ -3,5 +3,24 @@
 
 int main(void) 
 {
+    //Get input of the Height
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    } 
+    while (n < 1 || n > 8);
     
+    // Menampilkan pesan jika input valid
+    printf("So far good!\n");
+
+    // Mencetak output dalam format tabel persegi
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            printf("* "); // Mencetak bintang
+        }
+        printf("\n"); // Pindah ke baris berikutnya
+    }
 }
